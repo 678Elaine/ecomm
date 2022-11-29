71b751e60ee58d9c278123a5a2e2673e8e99928f
678Elaine <94685092+678Elaine@users.noreply.github.com>
2022-11-29 14:13:55 +0000

    Update cart.php

--- view/cart.php
+++ view/cart.php
@@ -11,6 +11,8 @@ $cart = list_all_cart_controller();
 <h2> Cart </h2>
 <?php
 
+if($cart>0)
+{
 echo
    "
 <h3> Cart </h3>
@@ -19,14 +21,12 @@ echo
 <thead>
     <tr>
         <th> Product Title  </th>
-        <th> Product Price</th>
-        <th> Product Image </th>
-        <th> Product Price </th>
-        <th> Product Quantity </th>
-        <th> Product Quantity </th>
-        <th> Product ID </th>
-        <th> Total Price</th>
-        <th> Remove </th>
+        <th> Product Image</th>
+        <th> Total Quantity </th>
+        <th> Unit Price </th>
+        <th> Total price </th>
+        <th> Manage Quantity </th>
+        <th> Remove Item</th>
         
 
     </tr>
@@ -38,10 +38,9 @@ echo
         <tbody>
         <tr>
         <td>".$x['product_title']."</td>
-        <td>".$x['product_price']."</td>
-        <td><img src='../images/product/".$x['product_image']."</td>
+        <td><img src='../images/product/".$x['product_image']."'></td>
         <td>".$x['qty']."</td>
-        <td>".$x['product_id']."</td>
+        <td>".$x['product_price']."</td>
         <td>".$totalprice."</td>
         <td> <a href = '../actions/managecartquantity.php?update_id=".$x['product_id']."'>Change Quantity </a></td>
         <td> <a href = '../functions/delete_fromcart.php?delete_id=".$x['product_id']."'> Remove from Cart </a></td>
@@ -50,7 +49,7 @@ echo
         ";
     }
 
-
+}
  ?>
 
  <button><a href = "paymentpage.php">Make Payment</a> </button>
